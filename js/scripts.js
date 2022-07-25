606889f3d55af6ea9518accaaa0cde2583952c71
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-07-25 20:33:44 -0300

    enviando projeto

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
